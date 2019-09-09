1847cace76dc4c7327ac4035a7efc0238ba0dc0e
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2019-09-09 06:44:39 -0400

    0.22

--- twopi.rb
+++ twopi.rb
@@ -4,7 +4,7 @@
 class Twopi < Formula
   desc "Package installer to setup Petra-M"
   homepage "http://piscope.psfc.mit.edu/index.php/Petra-M_(FEM_environment_on_MFEM)"
-  url "https://github.com/piScope/TwoPi/archive/0.21.tar.gz"
+  url "https://github.com/piScope/TwoPi/archive/0.22.tar.gz"
   sha256 "695117ea7a528de60bc64b0cc4a75bf73271efa67c79e6be79ec18a0862e75fd"
 
   # depends_on "cmake" => :build
