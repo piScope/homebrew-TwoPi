9b5f87f1856f6dee6d38f7ecd9c4c3992e1bbee2
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2020-04-20 11:37:37 -0400

    minor fix

--- twopi.rb
+++ twopi.rb
@@ -69,6 +69,7 @@ class Twopi < Formula
     
     ENV.prepend_path "PATH", "#{prefix}/bin"
     system "which python"
+    system "bin/twopi install HYPRE"
     
     if build.devel?
         system "bin/twopi install modules --PyMFEM-branch master --PetraM-Repo git@github.mit.edu:piScope  --piScope-branch master --PetraM-branch master --no-occ-gmsh --no-python_mod --log-dir #{prefix}/log"
