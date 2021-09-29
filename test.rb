3fa0b3efe20b15e8db05ff9f7904c3f719c41bc1
runner <runner@Mac-1632951763948.local>
2021-09-29 22:55:16 +0000

    test 0.0.7

--- test.rb
+++ test.rb
@@ -2,8 +2,8 @@ class Test < Formula
   desc "A module to test workflow"
   homepage "http://piscope.psfc.mit.edu/index.php/Petra-M_(FEM_environment_on_MFEM)"  
 
-  url "https://github.com/piScope/Test/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "d11d18af3ccda823edf3ae4a5725a52301cdd29db984f22220e9a693ba734ec6"  
+  url "https://github.com/piScope/Test/archive/refs/tags/v0.0.7.tar.gz"
+  sha256 "1f6bdd448fbea73fafd13c8f184085dfb9624ae0a264c615ffc01f297b55aa73"  
 
   depends_on "python@3.9"
   
