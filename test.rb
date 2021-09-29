5407e93b53235dfebe1a35e64e8c0b46e27af973
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2021-09-29 15:53:42 -0400

    udpate test.rb

--- test.rb
+++ test.rb
@@ -2,8 +2,8 @@ class Test < Formula
   desc "A module to test workflow"
   homepage "http://piscope.psfc.mit.edu/index.php/Petra-M_(FEM_environment_on_MFEM)"  
 
-  url "https://github.com/piScope/Test/archive/v0.0.0.tar.gz"
-  sha256 ""
+  url "https://github.com/piScope/Test/archive/v0.0.1.tar.gz"
+  sha256 "af7a1150c07775df3950b910f6974bd941d5e7fcbe8536cf3cc53b22d87016c0"  
 
   depends_on "python@3.9"
   
